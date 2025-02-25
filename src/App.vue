f8c6f6733a78dc3d16f1077a6c50ac0acaa3ba24
xb <b_xue@163.com>
2025-02-25 19:53:03 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="input-group flex-nowrap">
+        <span class="input-group-text" id="addon-wrapping">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
